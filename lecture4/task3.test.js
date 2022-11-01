fccb2a7fc7369386c84a5c21e71ef4bf3e1060b5
volodymyr.momot <volodymyr.momot@itomy.ch>
2022-11-01 17:26:50 +0200

    delete comment

--- lecture4/task3.test.js
+++ lecture4/task3.test.js
@@ -1,11 +1,3 @@
-// **Реалізувати функцію *createBase***
-//
-// **let addSix = createBase(6);**
-//
-// **addSix(10); // returns 16**
-//
-// **addSix(21); // returns 27**
-
 createBase = require('./task3');
 
 
