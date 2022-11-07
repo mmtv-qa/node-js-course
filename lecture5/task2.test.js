b70671b2e30722efe2051004c44ebad6b484cc74
volodymyr.momot <volodymyr.momot@itomy.ch>
2022-11-07 11:58:17 +0200

    add task2 implementation

---
+++ lecture5/task2.test.js
@@ -0,0 +1,23 @@
+Circle = require('./task2.js');
+
+describe('Circle', () => {
+    const test_circle = new Circle(50, 30, 10);
+
+    test('constructor work correctly', () => {
+        expect(test_circle.x).toBe(50);
+        expect(test_circle.y).toBe(30);
+        expect(test_circle.r).toBe(10);
+    })
+
+    test('getArea', () => {
+        expect(test_circle.getArea()).toBeCloseTo(314.1592653589793);
+    })
+
+    test('getPerimeter', () => {
+        expect(test_circle.getPerimeter()).toBeCloseTo(62.83185307179586);
+    })
+
+    test('getIntersectionArea', () => {
+        expect(test_circle.getIntersectionArea(new Circle(50, 30, 10))).toBeCloseTo(314.1592653589793);
+    })
+})
